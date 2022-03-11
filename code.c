61b47f81ad3799be9ac50b3790bfe3952988885d
Trubel <100468153+Emergin@users.noreply.github.com>
2022-03-11 16:47:01 +0300

    code
    grading system

---
+++ code.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(void){
+int marks;
+printf("enter your marks:");
+scanf("%d",&marks);
+if(marks<0|| marks >100)
+{
+printf("wrong entry");
+if(marks<50)
+{
+printf("grade F");
+}
+else if(marks>=50&&marks<60)
+{
+printf("grade D");
+}
+else if(marks>=60&&marks<70)
+{
+printf("grade C");{
+if(marks>=70&&marks<80)
+}
+printf("grade B");
+if(marks>=80&&marks<90)
+{
+printf("grade A");
+{
+else 
+}
+	printf("gradeA+");
+}
+return 0;
+}
